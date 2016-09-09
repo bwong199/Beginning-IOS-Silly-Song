ee02cce7f912c2c82df8f8fb4ead61c81bc640c3
bwong199 <bwong199@my.bcit.ca>
2016-09-09 13:27:03 -0600

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Ben Wong on 2016-09-09.
+//  Copyright © 2016 Ben Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
